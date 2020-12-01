258099738ecd811a41f844628fa87102255beee1
John Nguyen <aznxmist@gmail.com>
2020-12-01 11:57:03 -0600

    Website can load

--- src/components/UnitsPage.js
+++ src/components/UnitsPage.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
 
-function Units({units}) {
+function Units (props) {
+    
+    const units = props.units.map(unitInfo => {
+        return (
+            <div className="col" key={unitInfo.id}>
+                <RenderThumbnails units={unitInfo} />
+            </div>
+        );
+    });
+
+
     return (
         <div className="container">
             <div className="row">
                 <div className="col">
-                    {units.image.thumbawk}
+                    {units}
                 </div>
             </div>
         </div>
     );
+        
+
+}
+
+function RenderThumbnails ({units}) {
+    if(units) {
+        return (
+            <Media src={units.image.thumbawk} alt={units.name} width="75" height="75" />
+        );
+    }
+    else {
+        return (
+            <div />
+        );
+    }
 }
 
 export default Units;
