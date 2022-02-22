8c2cfcf685a3ac7a4e71bdb99e4743a6ca9c7625
John Nguyen <aznxmist@gmail.com>
2022-02-22 16:03:42 -0600

    Added Phantom Bullet and Cross Arts stats

--- src/components/unitinfo/StatsComponent.js
+++ src/components/unitinfo/StatsComponent.js
@@ -1,11 +1,12 @@
 import { Container, Row, Col, Media, Badge } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const lbIconSize = {
     maxHeight: 40,
     maxWidth: 40
 }
 
-export function RenderStats({unitName, unitStats, unitAtt, unitType, unitSkill, unitPassive, unitSlots}){
+export function RenderStats({unitName, unitStats, unitAtt, unitType, unitSkill, unitPassive, unitSlots, unitThumb}){
     //Tab for Stats
     //Possibly make if else statements to check if ability3 exists
     //TODO: Work on Viewports
@@ -19,21 +20,140 @@ export function RenderStats({unitName, unitStats, unitAtt, unitType, unitSkill, 
             if(unitSkill.liberation) {
                 return (
                     //Create tabs for Human and Liberation
+                    
                 );
             }
 
             //Check if Phantom Bullet Exists
             if(unitSkill.phantombullet) {
                 return (
-
+<Container>
+                            <Row>
+                                    <center><h1><strong>{unitName}</strong></h1></center>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <p><i>{unitAtt} , {unitType}</i></p>
+                                    <p>HP - {unitStats.hp} <Badge color="info" pill>+{unitStats.hpplus}</Badge></p>
+                                    <p>ATK - {unitStats.atk} <Badge color="info" pill>+{unitStats.atkplus}</Badge></p>
+                                    <p>DEF - {unitStats.def} <Badge color="info" pill>+{unitStats.defplus}</Badge></p>
+                                </Col>
+                                <Col>
+                                    <h3><strong>SLOTS</strong></h3>
+                                    <h4>{unitSlots.slot1}☆ <Media src={unitSlots.slot1type} style={lbIconSize} /></h4>
+                                    <h4>{unitSlots.slot2}☆ <Media src={unitSlots.slot2type} style={lbIconSize} /></h4>
+                                    <h4>{unitSlots.slot3}☆ <Media src={unitSlots.slot3type} style={lbIconSize} /></h4>
+                                </Col>
+                                <Col>
+                                    <h3><strong>SKILL</strong></h3>
+                                    <p><Badge color="primary" pill>BREAK {unitSkill.skillbreak}</Badge> {unitSkill.skill}</p>
+                                    <h3><strong>ARTS</strong></h3>
+                                    <p><Badge color="primary" pill>BREAK {unitSkill.artsbreak}</Badge> {unitSkill.arts}</p>
+                                    <h3><strong>TRUE ARTS</strong></h3>
+                                    <p><Badge color="primary" pill>BREAK {unitSkill.trueartsbreak}</Badge> {unitSkill.truearts}</p>
+                                    <h3><strong>PHANTOM BULLET</strong></h3>
+                                    <p><Badge color="primary" pill>BREAK {unitSkill.phantombulletbreak}</Badge> {unitSkill.phantombullet}</p>
+                                </Col>
+                                <Col>
+                                    <h3><strong>PASSIVES</strong></h3>
+                                    <ul>
+                                        <li>{unitPassive.ability1}</li>
+                                        <li>{unitPassive.ability2}</li>
+                                        <li>{unitPassive.ability3}</li>
+                                    </ul>
+                                </Col>
+                            </Row>
+                        </Container>
                 );
             }
 
             //Check if Cross Arts exists
             if(unitSkill.crossarts) {
-                return (
+                
+                //Check Virago
+                if(unitPassive.ability4) {
+                    return (
+                        <Container>
+                            <Row>
+                                    <center><h1><strong>{unitName}</strong></h1></center>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <p><i>{unitAtt} , {unitType}</i></p>
+                                    <p>HP - {unitStats.hp} <Badge color="info" pill>+{unitStats.hpplus}</Badge></p>
+                                    <p>ATK - {unitStats.atk} <Badge color="info" pill>+{unitStats.atkplus}</Badge></p>
+                                    <p>DEF - {unitStats.def} <Badge color="info" pill>+{unitStats.defplus}</Badge></p>
+                                </Col>
+                                <Col>
+                                    <h3><strong>SLOTS</strong></h3>
+                                    <h4>{unitSlots.slot1}☆ <Media src={unitSlots.slot1type} style={lbIconSize} /></h4>
+                                    <h4>{unitSlots.slot2}☆ <Media src={unitSlots.slot2type} style={lbIconSize} /></h4>
+                                    <h4>{unitSlots.slot3}☆ <Media src={unitSlots.slot3type} style={lbIconSize} /></h4>
+                                </Col>
+                                <Col>
+                                    <h3><strong>SKILL</strong></h3>
+                                    <p><Badge color="primary" pill>BREAK {unitSkill.skillbreak}</Badge> {unitSkill.skill}</p>
+                                    <h3><strong>ARTS</strong></h3>
+                                    <p><Badge color="primary" pill>BREAK {unitSkill.artsbreak}</Badge> {unitSkill.arts}</p>
+                                    <h3><strong>TRUE ARTS</strong></h3>
+                                    <p><Badge color="primary" pill>BREAK {unitSkill.trueartsbreak}</Badge> {unitSkill.truearts}</p>
+                                    <h3><strong>CROSS ARTS</strong></h3>
+                                    <p><Badge color="primary" pill>BREAK {unitSkill.crossartsbreak}</Badge> {unitSkill.crossarts}</p>
+                                </Col>
+                                <Col>
+                                    <h3><strong>PASSIVES</strong></h3>
+                                    <ul>
+                                        <li>{unitPassive.ability1}</li>
+                                        <li>{unitPassive.ability2}</li>
+                                        <li>{unitPassive.ability3}</li>
+                                        <li>{unitPassive.ability4}</li>
+                                    </ul>
+                                </Col>
+                            </Row>
+                        </Container>
+                    );
+                }
 
-                );
+                //3 Abilities
+                return (
+                        <Container>
+                            <Row>
+                                    <center><h1><strong>{unitName}</strong></h1></center>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <p><i>{unitAtt} , {unitType}</i></p>
+                                    <p>HP - {unitStats.hp} <Badge color="info" pill>+{unitStats.hpplus}</Badge></p>
+                                    <p>ATK - {unitStats.atk} <Badge color="info" pill>+{unitStats.atkplus}</Badge></p>
+                                    <p>DEF - {unitStats.def} <Badge color="info" pill>+{unitStats.defplus}</Badge></p>
+                                </Col>
+                                <Col>
+                                    <h3><strong>SLOTS</strong></h3>
+                                    <h4>{unitSlots.slot1}☆ <Media src={unitSlots.slot1type} style={lbIconSize} /></h4>
+                                    <h4>{unitSlots.slot2}☆ <Media src={unitSlots.slot2type} style={lbIconSize} /></h4>
+                                    <h4>{unitSlots.slot3}☆ <Media src={unitSlots.slot3type} style={lbIconSize} /></h4>
+                                </Col>
+                                <Col>
+                                    <h3><strong>SKILL</strong></h3>
+                                    <p><Badge color="primary" pill>BREAK {unitSkill.skillbreak}</Badge> {unitSkill.skill}</p>
+                                    <h3><strong>ARTS</strong></h3>
+                                    <p><Badge color="primary" pill>BREAK {unitSkill.artsbreak}</Badge> {unitSkill.arts}</p>
+                                    <h3><strong>TRUE ARTS</strong></h3>
+                                    <p><Badge color="primary" pill>BREAK {unitSkill.trueartsbreak}</Badge> {unitSkill.truearts}</p>
+                                    <h3><strong>CROSS ARTS</strong></h3>
+                                    <p><Badge color="primary" pill>BREAK {unitSkill.crossartsbreak}</Badge> {unitSkill.crossarts}</p>
+                                </Col>
+                                <Col>
+                                    <h3><strong>PASSIVES</strong></h3>
+                                    <ul>
+                                        <li>{unitPassive.ability1}</li>
+                                        <li>{unitPassive.ability2}</li>
+                                        <li>{unitPassive.ability3}</li>
+                                    </ul>
+                                </Col>
+                            </Row>
+                        </Container>
+                    );
             }
             
             //Specific if statement for Elmessio
@@ -158,4 +278,90 @@ export function RenderStats({unitName, unitStats, unitAtt, unitType, unitSkill, 
         );
     }
     return <div />
+}
+
+//Human version of Liberation Units Tab
+export function humanMode ({unitName, unitStats, unitAtt, unitType, unitSkill, unitPassive, unitSlots}) {
+    return (
+        <Container>
+            <Row>
+                    <center><h1><strong>{unitName}</strong></h1></center>
+            </Row>
+            <Row>
+                <Col>
+                    <p><i>{unitAtt} , {unitType}</i></p>
+                    <p>HP - {unitStats.hp} <Badge color="info" pill>+{unitStats.hpplus}</Badge></p>
+                    <p>ATK - {unitStats.atk} <Badge color="info" pill>+{unitStats.atkplus}</Badge></p>
+                    <p>DEF - {unitStats.def} <Badge color="info" pill>+{unitStats.defplus}</Badge></p>
+                </Col>
+                <Col>
+                    <h3><strong>SLOTS</strong></h3>
+                    <h4>{unitSlots.slot1}☆ <Media src={unitSlots.slot1type} style={lbIconSize} /></h4>
+                    <h4>{unitSlots.slot2}☆ <Media src={unitSlots.slot2type} style={lbIconSize} /></h4>
+                    <h4>{unitSlots.slot3}☆ <Media src={unitSlots.slot3type} style={lbIconSize} /></h4>
+                </Col>
+                <Col>
+                    <h3><strong>SKILL</strong></h3>
+                    <p><Badge color="primary" pill>BREAK {unitSkill.skillbreak}</Badge> {unitSkill.skill}</p>
+                    <h3><strong>ARTS</strong></h3>
+                    <p><Badge color="primary" pill>BREAK {unitSkill.artsbreak}</Badge> {unitSkill.arts}</p>
+                    <h3><strong>TRUE ARTS</strong></h3>
+                    <p><Badge color="primary" pill>BREAK {unitSkill.trueartsbreak}</Badge> {unitSkill.truearts}</p>
+                    <h3><strong>CROSS ARTS</strong></h3>
+                    <p><Badge color="primary" pill>BREAK {unitSkill.crossartsbreak}</Badge> {unitSkill.crossarts}</p>
+                </Col>
+                <Col>
+                    <h3><strong>PASSIVES</strong></h3>
+                    <ul>
+                        <li>{unitPassive.ability1}</li>
+                        <li>{unitPassive.ability2}</li>
+                        <li>{unitPassive.ability3}</li>
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+//Liberation Skill Tab
+export function liberationMode ({unitName, unitStats, unitAtt, unitType, unitSkill, unitPassive, unitSlots}) {
+    return (
+        <Container>
+            <Row>
+                    <center><h1><strong>{unitName}</strong></h1></center>
+            </Row>
+            <Row>
+                <Col>
+                    <p><i>{unitAtt} , {unitType}</i></p>
+                    <p>HP - {unitStats.hp} <Badge color="info" pill>+{unitStats.hpplus}</Badge></p>
+                    <p>ATK - {unitStats.atk} <Badge color="info" pill>+{unitStats.atkplus}</Badge></p>
+                    <p>DEF - {unitStats.def} <Badge color="info" pill>+{unitStats.defplus}</Badge></p>
+                </Col>
+                <Col>
+                    <h3><strong>SLOTS</strong></h3>
+                    <h4>{unitSlots.slot1}☆ <Media src={unitSlots.slot1type} style={lbIconSize} /></h4>
+                    <h4>{unitSlots.slot2}☆ <Media src={unitSlots.slot2type} style={lbIconSize} /></h4>
+                    <h4>{unitSlots.slot3}☆ <Media src={unitSlots.slot3type} style={lbIconSize} /></h4>
+                </Col>
+                <Col>
+                    <h3><strong>LIBERATION SKILLS</strong></h3>
+                    <ul>
+                        <li><Badge color="primary" pill>BREAK {unitSkill.skill1break}</Badge> {unitSkill.skill1}</li>
+                        <li><Badge color="primary" pill>BREAK {unitSkill.skill2break}</Badge> {unitSkill.skill2}</li>
+                        <li><Badge color="primary" pill>BREAK {unitSkill.skill3break}</Badge> {unitSkill.skill3}</li>
+                    </ul>
+                    <h3><strong>MEGA SKILL</strong></h3>
+                    <p><Badge color="primary" pill>BREAK {unitSkill.megaskillbreak}</Badge> {unitSkill.megaskill}</p>
+                </Col>
+                <Col>
+                    <h3><strong>PASSIVES</strong></h3>
+                    <ul>
+                        <li>{unitPassive.ability1}</li>
+                        <li>{unitPassive.ability2}</li>
+                        <li>{unitPassive.ability3}</li>
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
