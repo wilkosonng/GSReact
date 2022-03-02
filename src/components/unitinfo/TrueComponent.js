bce63b3814869e7530c54d7ae549d859137898e5
John Nguyen <aznxmist@gmail.com>
2022-03-02 13:17:35 -0600

    Fixed pathing for multiple tw units

--- src/components/unitinfo/TrueComponent.js
+++ src/components/unitinfo/TrueComponent.js
@@ -1,4 +1,5 @@
 import { Container, Row, Col, Badge, Media, NavItem, NavLink, Nav, TabContent, TabPane } from 'reactstrap';
+import React, { useState } from 'react';
 
 const imageSize ={
     maxHeight: 500,
@@ -6,9 +7,6 @@ const imageSize ={
 };
 
 export function RenderTrue({unitTrue}) {
-    //Tab for True Weapon
-    //Add if statements to check to Passives based on amount (1 Passive)
-    //Add if statements to check multiple true weapons and create tabs based on number https://www.geeksforgeeks.org/reactjs-reactstrap-tab-component/
     //Hook for Tab State
     const [currentTab, setCurrentTab] = useState('1');
 
@@ -18,27 +16,27 @@ export function RenderTrue({unitTrue}) {
     }
 
     //Three TWs (2 Passive each)
-    if(unitTrue.true3) {
+    if(unitTrue.trueweapon.true3) {
         return (
             <React.Fragment>
                 <Nav tabs>
                     <NavItem>
                         <NavLink className="active"
                             onClick={() => {toggle('1')}} >
-                                <Media src={unitTrue.true1.thumb} object-fit="cover" id="unitTrue"/>
+                                <Media src={unitTrue.trueweapon.true1.thumb} object-fit="cover" id="unitTrue"/>
                         </NavLink>
                         
                     </NavItem>
                     <NavItem>
                         <NavLink className=""
                             onClick={() => {toggle('2')}} >
-                                <Media src={unitTrue.true2.thumb} object-fit="cover" id="unitTrue"/>
+                                <Media src={unitTrue.trueweapon.true2.thumb} object-fit="cover" id="unitTrue"/>
                         </NavLink>
                     </NavItem>
                     <NavItem>
                         <NavLink className=""
                             onClick={() => {toggle('3')}} >
-                                <Media src={unitTrue.true3.thumb} object-fit="cover" id="unitTrue"/>
+                                <Media src={unitTrue.trueweapon.true3.thumb} object-fit="cover" id="unitTrue"/>
                         </NavLink>
                     </NavItem>
                 </Nav>
@@ -46,17 +44,17 @@ export function RenderTrue({unitTrue}) {
                     <TabPane tabId="1">
                         <Container>
                             <Row>
-                                <center><Media src={unitTrue.true1.detail} style={imageSize} /></center>
-                                <center><strong><h1><Media src={unitTrue.true1.slot} />{unitTrue.true1.name}<Media src={unitTrue.true1.slot} /></h1></strong></center>
+                                <center><Media src={unitTrue.trueweapon.true1.detail} style={imageSize} /></center>
+                                <center><strong><h1><Media src={unitTrue.trueweapon.true1.slot} />{unitTrue.trueweapon.true1.name}<Media src={unitTrue.trueweapon.true1.slot} /></h1></strong></center>
                                 <Col>
                                     <h3><strong>SKILL</strong></h3>
-                                    <p><Badge color="secondary">BREAK {unitTrue.true1.skillbreak}</Badge> {unitTrue.true1.skill} </p>
+                                    <p><Badge color="secondary">BREAK {unitTrue.trueweapon.true1.skillbreak}</Badge> {unitTrue.trueweapon.true1.skill} </p>
                                 </Col>
                                 <Col>
                                     <p><h3><strong>PASSIVE</strong></h3>
                                         <ul>
-                                            <li>{unitTrue.true1.passive.ability1}</li>
-                                            <li>{unitTrue.true1.passive.ability2}</li>
+                                            <li>{unitTrue.trueweapon.true1.passive.ability1}</li>
+                                            <li>{unitTrue.trueweapon.true1.passive.ability2}</li>
                                         </ul>
                                     </p>
                                 </Col>
@@ -66,17 +64,17 @@ export function RenderTrue({unitTrue}) {
                     <TabPane tabId="2">
                         <Container>
                             <Row>
-                                <center><Media src={unitTrue.true2.detail} style={imageSize} /></center>
-                                <center><strong><h1><Media src={unitTrue.true2.slot} />{unitTrue.true2.name}<Media src={unitTrue.true2.slot} /></h1></strong></center>
+                                <center><Media src={unitTrue.trueweapon.true2.detail} style={imageSize} /></center>
+                                <center><strong><h1><Media src={unitTrue.trueweapon.true2.slot} />{unitTrue.trueweapon.true2.name}<Media src={unitTrue.trueweapon.true2.slot} /></h1></strong></center>
                                 <Col>
                                     <h3><strong>SKILL</strong></h3>
-                                    <p><Badge color="secondary">BREAK {unitTrue.true2.skillbreak}</Badge> {unitTrue.true2.skill} </p>
+                                    <p><Badge color="secondary">BREAK {unitTrue.trueweapon.true2.skillbreak}</Badge> {unitTrue.trueweapon.true2.skill} </p>
                                 </Col>
                                 <Col>
                                     <p><h3><strong>PASSIVE</strong></h3>
                                         <ul>
-                                            <li>{unitTrue.true2.passive.ability1}</li>
-                                            <li>{unitTrue.true2.passive.ability2}</li>
+                                            <li>{unitTrue.trueweapon.true2.passive.ability1}</li>
+                                            <li>{unitTrue.trueweapon.true2.passive.ability2}</li>
                                         </ul>
                                     </p>
                                 </Col>
@@ -86,17 +84,17 @@ export function RenderTrue({unitTrue}) {
                     <TabPane tabId="3">
                         <Container>
                             <Row>
-                                <center><Media src={unitTrue.true3.detail} style={imageSize} /></center>
-                                <center><strong><h1><Media src={unitTrue.true3.slot} />{unitTrue.true3.name}<Media src={unitTrue.true3.slot} /></h1></strong></center>
+                                <center><Media src={unitTrue.trueweapon.true3.detail} style={imageSize} /></center>
+                                <center><strong><h1><Media src={unitTrue.trueweapon.true3.slot} />{unitTrue.trueweapon.true3.name}<Media src={unitTrue.trueweapon.true3.slot} /></h1></strong></center>
                                 <Col>
                                     <h3><strong>SKILL</strong></h3>
-                                    <p><Badge color="secondary">BREAK {unitTrue.true3.skillbreak}</Badge> {unitTrue.true3.skill} </p>
+                                    <p><Badge color="secondary">BREAK {unitTrue.trueweapon.true3.skillbreak}</Badge> {unitTrue.trueweapon.true3.skill} </p>
                                 </Col>
                                 <Col>
                                     <p><h3><strong>PASSIVE</strong></h3>
                                         <ul>
-                                            <li>{unitTrue.true3.passive.ability1}</li>
-                                            <li>{unitTrue.true3.passive.ability2}</li>
+                                            <li>{unitTrue.trueweapon.true3.passive.ability1}</li>
+                                            <li>{unitTrue.trueweapon.true3.passive.ability2}</li>
                                         </ul>
                                     </p>
                                 </Col>
@@ -109,21 +107,21 @@ export function RenderTrue({unitTrue}) {
     }
 
     //Two TWs (2 Passive each)
-    if(unitTrue.true2) {
+    if(unitTrue.trueweapon.true2) {
         return (
             <React.Fragment>
                 <Nav tabs>
                     <NavItem>
                         <NavLink className="active"
                             onClick={() => {toggle('1')}} >
-                                <Media src={unitTrue.true1.thumb} object-fit="cover" id="unitTrue"/>
+                                <Media src={unitTrue.trueweapon.true1.thumb} object-fit="cover" id="unitTrue"/>
                         </NavLink>
                         
                     </NavItem>
                     <NavItem>
                         <NavLink className=""
                             onClick={() => {toggle('2')}} >
-                                <Media src={unitTrue.true2.thumb} object-fit="cover" id="unitTrue"/>
+                                <Media src={unitTrue.trueweapon.true2.thumb} object-fit="cover" id="unitTrue"/>
                         </NavLink>
                     </NavItem>
                 </Nav>
@@ -131,17 +129,17 @@ export function RenderTrue({unitTrue}) {
                     <TabPane tabId="1">
                         <Container>
                             <Row>
-                                <center><Media src={unitTrue.true1.detail} style={imageSize} /></center>
-                                <center><strong><h1><Media src={unitTrue.true1.slot} />{unitTrue.true1.name}<Media src={unitTrue.true1.slot} /></h1></strong></center>
+                                <center><Media src={unitTrue.trueweapon.true1.detail} style={imageSize} /></center>
+                                <center><strong><h1><Media src={unitTrue.trueweapon.true1.slot} />{unitTrue.trueweapon.true1.name}<Media src={unitTrue.trueweapon.true1.slot} /></h1></strong></center>
                                 <Col>
                                     <h3><strong>SKILL</strong></h3>
-                                    <p><Badge color="secondary">BREAK {unitTrue.true1.skillbreak}</Badge> {unitTrue.true1.skill} </p>
+                                    <p><Badge color="secondary">BREAK {unitTrue.trueweapon.true1.skillbreak}</Badge> {unitTrue.trueweapon.true1.skill} </p>
                                 </Col>
                                 <Col>
                                     <p><h3><strong>PASSIVE</strong></h3>
                                         <ul>
-                                            <li>{unitTrue.true1.passive.ability1}</li>
-                                            <li>{unitTrue.true1.passive.ability2}</li>
+                                            <li>{unitTrue.trueweapon.true1.passive.ability1}</li>
+                                            <li>{unitTrue.trueweapon.true1.passive.ability2}</li>
                                         </ul>
                                     </p>
                                 </Col>
@@ -151,17 +149,17 @@ export function RenderTrue({unitTrue}) {
                     <TabPane tabId="2">
                         <Container>
                             <Row>
-                                <center><Media src={unitTrue.true2.detail} style={imageSize} /></center>
-                                <center><strong><h1><Media src={unitTrue.true2.slot} />{unitTrue.true2.name}<Media src={unitTrue.true2.slot} /></h1></strong></center>
+                                <center><Media src={unitTrue.trueweapon.true2.detail} style={imageSize} /></center>
+                                <center><strong><h1><Media src={unitTrue.trueweapon.true2.slot} />{unitTrue.trueweapon.true2.name}<Media src={unitTrue.trueweapon.true2.slot} /></h1></strong></center>
                                 <Col>
                                     <h3><strong>SKILL</strong></h3>
-                                    <p><Badge color="secondary">BREAK {unitTrue.true2.skillbreak}</Badge> {unitTrue.true2.skill} </p>
+                                    <p><Badge color="secondary">BREAK {unitTrue.trueweapon.true2.skillbreak}</Badge> {unitTrue.trueweapon.true2.skill} </p>
                                 </Col>
                                 <Col>
                                     <p><h3><strong>PASSIVE</strong></h3>
                                         <ul>
-                                            <li>{unitTrue.true2.passive.ability1}</li>
-                                            <li>{unitTrue.true2.passive.ability2}</li>
+                                            <li>{unitTrue.trueweapon.true2.passive.ability1}</li>
+                                            <li>{unitTrue.trueweapon.true2.passive.ability2}</li>
                                         </ul>
                                     </p>
                                 </Col>
