f7604fc3f88e6aa8eade4a3ac15c036deb1cfc3e
John Nguyen <aznxmist@gmail.com>
2022-04-05 16:14:07 -0500

    Added section for Newest News, and then Older news

--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -22,29 +22,69 @@ export default function News () {
             <Row>
                 <Card style={{backgroundColor:"#464866", color: "#C5C6C7"}}>
                     <CardBody>
-                        <CardGroup>
+                        <Card style={{backgroundColor:"#464866", color: "#C5C6C7", border: "none"}}>
+                            <CardHeader style={{backgroundColor: "#aaabb8", color: "#29648a", marginBottom: "1rem"}}>
+                                    <center><h1>Newest News</h1></center>
+                            </CardHeader>
+                        
                             {newsTopics.map(theNews => {
                                 const theArticle = theNews.article;
-                                return (
-                                    <React.Fragment key={theNews.id}>
-                                        <Col>
-                                            <Link to={`/news/${theNews.id}`} style={{textDecoration: 'none'}} key={theNews.id}>
-                                                <Card style={{width: "300px", height: "300px", backgroundColor: "#aaabb8", color: "#29648a"}}>
-                                                    <CardHeader>
-                                                        <h3><center><strong>{theNews.title}</strong></center></h3>
-                                                    </CardHeader>
-                                                    <CardBody>
-                                                        <div dangerouslySetInnerHTML={{ __html: theArticle }} />
-                                                    </CardBody>
-                                                </Card>
-                                            </Link>
-                                        </Col>
-                                    </React.Fragment>
-                                    
-                                )
-                                
+                                if(theNews.new){
+                                    return (
+                                        <React.Fragment key={theNews.id}>
+                                            <Col xs="12">
+                                                <center>
+                                                    <Link to={`/news/${theNews.id}`} style={{textDecoration: 'none'}} key={theNews.id}>
+                                                        <Card style={{height: "300px", maxWidth: "1000rem", backgroundColor: "#25274d", color: "#29648a", overflow: "hidden", marginBottom: "5rem"}}>
+                                                            <CardHeader  style={{backgroundColor: "#29648a", color: "#aaabb8"}}>
+                                                                <h3><center><strong>{theNews.title}</strong></center></h3>
+                                                            </CardHeader>
+                                                            <CardBody>
+                                                                <center><div dangerouslySetInnerHTML={{ __html: theArticle }} /></center>
+                                                            </CardBody>
+                                                        </Card>
+                                                    </Link>
+                                                </center>
+                                            </Col>
+                                        </React.Fragment>
+                                    )
+                                }
+
                             })}
-                        </CardGroup>
+                            </Card>
+
+                            <Card style={{backgroundColor:"#464866", color: "#C5C6C7", border: "none"}}>
+                                <CardHeader style={{backgroundColor: "#aaabb8", color: "#29648a"}}>
+                                    <center><h1>Older News</h1></center>
+                                </CardHeader>
+                                <CardBody>
+                                <CardGroup>
+                                    {newsTopics.map(theNews => {
+                                    const theArticle = theNews.article;
+
+                                    if(!theNews.new){
+                                        return (
+                                            <React.Fragment key={theNews.id}>
+                                                <Col>
+                                                    <Link to={`/news/${theNews.id}`} style={{textDecoration: 'none'}} key={theNews.id}>
+                                                        <Card style={{height: "300px", backgroundColor: "#25274d", color: "#29648a", overflow: "hidden", marginBottom: "1rem"}}>
+                                                            <CardHeader style={{backgroundColor: "#29648a", color: "#aaabb8"}}>
+                                                                <h3><center><strong>{theNews.title}</strong></center></h3>
+                                                            </CardHeader>
+                                                            <CardBody>
+                                                                <center><div dangerouslySetInnerHTML={{ __html: theArticle }} /></center>
+                                                            </CardBody>
+                                                        </Card>
+                                                    </Link>
+                                                </Col>
+                                            </React.Fragment>
+                                        )
+                                    }
+
+                                    })}
+                                </CardGroup>
+                                </CardBody>
+                            </Card>
                     </CardBody>
                 </Card>
             </Row>
