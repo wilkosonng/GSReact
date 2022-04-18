cdbac9e82b3817b04068df4b94a08a4c637df8ca
John Nguyen <aznxmist@gmail.com>
2022-04-18 15:01:20 -0500

    Made Banner responsive. Rearranged Navbar links. Added HOME next to Rolly

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -31,6 +31,13 @@ const Rolly = styled.div`
 `
 
 //<Media src="/chara_face_1200_1.png" style={{maxHeight: "40px"}} />
+/*
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/equipment" activeClassName="active">
+                                        <b><p style={{paddingTop: "3px", marginBottom: "0rem"}}>EQUIPMENT</p></b>
+                                    </NavLink>
+                                </NavItem>
+*/
 return (
     <React.Fragment>
         <Container>
@@ -38,7 +45,7 @@ return (
                 <Col>
                     <center>
                         <a href="/" >
-                            <Media src="/Banner.png" style={{height: "400px" , paddingTop: "5rem", paddingBottom: "1rem"}}/>
+                            <Media src="/Banner.png" style={{maxHeight: "400px", width: "100%" , paddingTop: "5rem", paddingBottom: "1rem"}}/>
                         </a>
                     </center>
                 </Col>
@@ -52,6 +59,7 @@ return (
                                 <NavItem>
                                     <NavLink className="nav-link" to="/" activeClassName="active">
                                         <Media src="/chara_face_1200_1.png" style={{maxHeight: "35px", marginTop: "0rem"}} />
+                                        <b>HOME</b>
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
@@ -60,18 +68,13 @@ return (
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/units" activeClassName="active">
-                                        <b><p style={{paddingTop: "3px", marginBottom: "0rem"}}>UNITS</p></b>
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
                                     <NavLink className="nav-link" to="/tierlist" activeClassName="active">
                                         <b><p style={{paddingTop: "3px", marginBottom: "0rem"}}>TIERLIST</p></b>
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/equipment" activeClassName="active">
-                                        <b><p style={{paddingTop: "3px", marginBottom: "0rem"}}>EQUIPMENT</p></b>
+                                    <NavLink className="nav-link" to="/units" activeClassName="active">
+                                        <b><p style={{paddingTop: "3px", marginBottom: "0rem"}}>UNITS</p></b>
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
