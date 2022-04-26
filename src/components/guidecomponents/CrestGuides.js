5dd02e3257db4e55c07bd2a354ddbdfee1600cfe
John Nguyen <aznxmist@gmail.com>
2022-04-26 13:57:11 -0500

    Added dungeon banners as the dungeon selection

--- src/components/guidecomponents/CrestGuides.js
+++ src/components/guidecomponents/CrestGuides.js
@@ -19,27 +19,19 @@ export default function CrestGuides () {
             <Row>
                 <Card style={{backgroundColor:"#464866", color: "#C5C6C7"}}>
                     <CardBody>
-                            <Card style={{backgroundColor:"#464866", color: "#C5C6C7", border: "none"}}>
-                                <CardBody>
-                                <CardGroup>
-                                    {guideTopics.map(theGuides => {
-                                        return (
-                                            <React.Fragment key={theGuides.id}>
-                                                <Col>
-                                                    <Link to={`/guides/crest-guides/${theGuides.link}`} style={{textDecoration: 'none'}} key={theGuides.id}>
-                                                        <Card style={{height: "auto", backgroundColor: "#25274d", color: "#aaabb8", marginBottom: "1rem"}}>
-                                                            <p>{theGuides.title}</p>
-                                                            <center><p style={{backgroundColor: "#29648a", color: "#aaabb8", maxWidth: "200px", float: "right", marginTop: "3rem", marginRight: "1rem", width: "100%", height: "auto"}}>Read More</p></center>
-                                                        </Card>
-                                                    </Link>
-                                                </Col>
-                                            </React.Fragment>
-                                        )
-                                    }
-                                    )}
-                                </CardGroup>
-                                </CardBody>
-                            </Card>
+                        {guideTopics.map(theGuides => {
+                            return (
+                                <React.Fragment key={theGuides.id}>
+                                    <Col lg="auto">
+                                        <center>
+                                        <Link to={`/guides/crest-guides/${theGuides.link}`} style={{textDecoration: 'none'}} key={theGuides.id}>
+                                            <img src={theGuides.banner} style={{height: "auto", width: "100%", maxWidth: "400px"}}/>
+                                        </Link></center>
+                                    </Col>
+                                </React.Fragment>
+                            )
+                        }
+                        )}
                     </CardBody>
                 </Card>
             </Row>
