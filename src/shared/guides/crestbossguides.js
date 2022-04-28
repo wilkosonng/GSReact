8aec47701d11a4daf2e2b082993153980524b753
John Nguyen <aznxmist@gmail.com>
2022-04-28 13:55:21 -0500

    Added Freydal and Goldola Crests

--- src/shared/guides/crestbossguides.js
+++ src/shared/guides/crestbossguides.js
@@ -594,6 +594,269 @@ const CrestBossGuides = [
                 </li>
         `,
         author: "ebouliben#1234, Jeffers#7056"
+    },
+    {
+        id: 5,
+        title: "Shrine of Diamond Dust",
+        link: "shrine-of-diamond-dust",
+        banner: "/db/CrestGuides/Crest_Banner_Freydal.png",
+        article: `
+            <center>
+                <b>Subzero Stormraptor</b> [EXTREME] <br />
+                <img src="/db/CrestGuides/Crest_Freydal.png" />
+                <br/>
+                BOSS: Freydal <br />
+
+                <b>Appears on Day: Wednesday [PST]</b> <br/>
+                HP: 5,000,000 <br />
+                Element: Water <br />
+                Race: Beast <br />
+                50% Resistance to Blind <br />
+                1000% Resistance to Paralysis/Faint/Seal/Freeze <br />
+            </center><br />
+            <h4><center><u>Gimmicks</u></center></h4>
+            <ul>
+                <li><b>Area Effect</b>: Icicle Cage - Reduce all Stats by 30%. Reset Skill gauge and reduce Skill CT Speed by 80% every 10s.</li>
+                <ul>
+                    <li>The area effect essentially renders all unit skills useless as they will not be able to refresh in time. Furthermore, the 30% Stats DOWN debuff applied throughout leave most units at 6-7k HP.</li>
+                </ul>
+                <li>ATK & CRI DMG UP</li>
+                <ul>
+                    <li>Self-explanatory, Freydal will hit harder due to these buffs, which it gains at the start of the fight.</li>
+                </ul>
+                <li><b>Storm Mode</b>: - Evasion Rate UP & Break Negated, cancelled when Bar is broken by status ailment DMG</li>
+                <ul>
+                    <li>It is recommended to reduce Freydal’s Evasion by ~60% or increase unit(s)’ Accuracy rate by ~60%. </li>
+                    <li>Alternatively, running a Poison/Burn inflictor will help damage Freydal’s bar. After its bar is broken, Freydal will lose its evasion buffs.</li>
+                    <li><b>NOTE:</b>  Freydal will re-enter into Storm Mode after using Blau Vortex</li>
+                </ul>
+                <li>At 70% HP threshold, gains Action Speed UP & immediately uses a random Arts</li>
+                <ul>
+                    <li>Self-explanatory, Freydal will attack faster past this point. Do be prepared for one of its 3 Arts as well.</li>
+                </ul>
+                <li>After 50% HP threshold, enter <b>Ultra Storm Mode</b> Evasion Rate UP & Break Negated, cancelled when Bar is broken by status ailment DMG</li>
+                <ul>
+                    <li>Ultra Storm Mode is a stronger version of Storm Mode. </li>
+                    <li>Units will require &gt;100% Accuracy buff to have a chance at landing a hit on Freydal past this point. </li>
+                    <li>Running a Poison/Burn inflictor will still be able to damage Freydal’s bar, causing Freydal to lose its Evasion buffs after its bar is broken.</li>
+                    <li><b>NOTE:</b> Freydal will re-enter into Ultra Storm Mode after using Blau Vortex
+                </ul>
+                <li>At 40% HP threshold, gains DMG UP buff, inflicts DMG RES DOWN debuff & immediately uses a random Arts</li>
+                <ul>
+                    <li>After this point, do not try to stall the fight longer than needed. The DMG UP buff gained by Freydal paired with the DMG RES debuff inflicted will cause units to take noticeable increased DMG. As such, its random Arts at this point will also be stronger than before, so make sure to be prepared.</li>
+                </ul>
+                <li>At 20% HP threshold, fills own Arts gauge at double the speed</li>
+                <ul>
+                    <li>Self-explanatory, Freydal now fills 2 orbs at a time.</li>
+                </ul>
+            </ul>
+
+            <h4><center><u>Normal Attacks</u></center></h4>
+            <b>Used during Storm/Ultra Strom Mode:</b>
+            <ul>
+                <li>Dives down using claws - Chance to Poison enemies</li>
+                <ul>
+                    <li>Coupled with the unique area effect reducing all allies’ Stats by 30%, leaving Poison DMG unchecked without adequate Healing will be very disastrous as the run progresses. As such, it is highly recommended to counter Freydal’s Poison with some form of healing.</li>
+                </ul>
+                <li>Flaps wings & fires ice projectiles - Chance to Freeze enemies</li>
+                <ul>
+                    <li>Although its Freeze only lasts for a few seconds, be wary that frozen units are unable to use any skills, arts or equipment.</li>
+                </ul>
+            </ul>
+            <b>Used while not in Storm/Ultra Strom Mode:</b>
+            <ul>
+                <li>Howl - For 8s, Reduce Enemies' Evasion Rate</li>
+                <ul>
+                    <li>It is only used before Freydal re-enters into Storm/Ultra Storm Mode. It does moderate single-hit DMG and can be lethal, especially right after its hard-hitting Blau Vortex Arts.</li>
+                </ul>
+                <li>Peck - Gains lifesteal for a few seconds</li>
+                <ul>
+                    <li>Deals insignificant damage, HP gained is also quite negligible.</li>
+                </ul>
+                <li>Scream - For 4s, ignore enemies' DEF</li>
+                <ul>
+                    <li>Deals insignificant damage.</li>
+                </ul>
+                <li>Stomp - Chance to Faint enemies</li>
+                <ul>
+                    <li>Deals insignificant damage.</li>
+                </ul>
+            </ul>
+
+            <center><h4><u>Arts</u></h4></center>
+
+            <ul>
+                <li><b>Raging Blizzard (PHY)</b> - Becomes invulnerable for a few seconds, disappears, and performs a dive attack</li>
+                <ul>
+                    <li>Deals moderate DMG. Nothing special about this Arts, besides the DMG dealt.</li>
+                </ul>
+                <li><b>Dart Wing (PHY)</b> - Chance to Poison enemies</li>
+                <ul>
+                    <li>Deals moderate DMG. Do note that this Poison is able to stack with the Poison inflicted from one of Freydal’s above mentioned normal attacks as well.</li>
+                </ul>
+                <li><b>Blau Vortex (MAG)</b> - Increases own CRI rate by 100% for the duration of this Arts, chance to Freeze at the end</li>
+                <ul>
+                    <li>Deals heavy DMG. Make sure to be prepared with some form of DMG RES and/or healing as the Freeze inflicted after might cause issues.</li>
+                    <li>Additionally, Freydal will perform its Howl move after this Arts. From testing, this Arts will <b>always</b> be used when Freydal is out of Storm/Ultra Storm Mode.</li>
+                </ul>
+            </ul>
+
+            <h3><center>Strategy</center></h3>
+            <p><center>
+            This crest boss is NOT recommended for newer players. Being one of the hardest currently available crest bosses in Global, 
+            it requires a well developed unit and equipment box. A general tip would be to either use Accuracy buffs/Evasion debuffs 
+            to counter Freydal’s high evasion or use a Burn/Poison unit to slowly chip away at its bar.
+            Nuke compositions will not be detailed here due to the high requirements needed.
+            </p></center>
+            <b><h4><u>Recommended Units</u></h4></b>
+            <ul>
+                <li>
+                    <b>Miranda/Thetis</b>
+                    <p>The best tanks for this stage if you decide to run one. 
+                    With Miranda’s MAG RES passive, it allows her to tank Freydal’s Blau 
+                    Vortex Arts much easier. Additionally, with her TW equipped, she gains 
+                    passive 100% Freeze RES, allowing her to be unaffected by Freydal’s Freeze. 
+                    On the other hand, Thetis is also a great option. With his TW equipped, 
+                    he also gains passive 100% Freeze RES, along with the added benefit of 
+                    being able to cleanse Poison from himself using his TA. His only slight 
+                    downside would be the extra Arts needed to get him into Magia Drive, 
+                    however this can be easily managed with proper equipment.</i>
+                    </p>
+                </li>
+                <li>
+                    <b>Hart/Mako/Asirpa/Cestina &gt; Sakura Mira</b>
+                    <p>These units are great ArtGen which also help provide safety to the team in various ways. 
+                    Hart and Mako both have great ArtGen capabilities, while being able to provide consistent 
+                    Healing over time to the team, with Mako having the added benefit of boosting teamwide DMG. 
+                    Asirpa functions similarly to the earlier two, but also provides a nice DEF boost for 
+                    extra safety. Cestina is able to provide great ArtGen, while having a 5☆ DEF slot and 
+                    being able to put up a barrier for the team. Lastly, Sakura Mira also works fine, 
+                    although she contributes by providing Healing over time and a barrier instead of ArtGen.</p>
+                </li>
+                <li>
+                    <b>Juno &gt;&gt;&gt; Shion </b>
+                    <p>As usual, Juno is the queen for this stage, gaining a whopping 100% 
+                    Accuracy UP after entering Demonic True Eclipse. To add on, she is also 
+                    able to generate a considerable amount of Arts for the team as long as she 
+                    has sufficient support to counteract her HP drain over time. Shion, on the 
+                    other hand, also gains 100% Accuracy UP on her TA. However, she is a pure DPS, 
+                    and does not help generate Arts for the team unlike Juno, which makes her a much 
+                    less desirable option. DO NOTE that additional Accuracy buffs and/or Evasion 
+                    debuffs are needed to damage Freydal after its 50% threshold.</p>
+                </li>
+                <li>
+                    <b>Victoire/Zeela</b>
+                    <p>When running a DPS without enough innate Accuracy, additional Accuracy buffs/Evasion 
+                    debuffs will be needed to damage Freydal past 50% HP. Considering the absence of equipment 
+                    and/or units with teamwide Accuracy UP passives, these units are solid options for 
+                    boosting Accuracy. Victoire requires the DPS to be a Light elemental in order to take 
+                    full advantage of her buffs on True Arts. On the other hand, Zeela is able to provide 
+                    teamwide CRI rate, Accuracy, and MAG DMG buffs from her NA.</p>
+                </li>
+                <li>
+                    <b>Pola &gt; Angelas &gt; Keit </b>
+                    <p>If you don’t want to deal with the annoying evasion gimmick of Freydal, 
+                    these units are great picks for chipping away at its bar over time. Pola 
+                    would be the best pick, dealing consistent and noticeable Poison DMG 
+                    especially when Freydal’s HP is high. Angelas is also a decent pick, 
+                    being able to inflict Burn from her skill and reduce Burn RES from her Arts. 
+                    Lastly, Keit can also work, however, as he is an unawoken unit, more support 
+                    will be required with him on the team.</p>
+                </li>
+        `,
+        author: "ebouliben#1234, Jeffers#7056"
+    },
+    {
+        id: 6,
+        title: "Shrine of Aberration",
+        link: "shrine-of-aberration",
+        banner: "/db/CrestGuides/Crest_Banner_Goldola.png",
+        article: `
+            <center>
+                <b>The Brutal Botanical Beast</b> [PRO] <br />
+                <img src="/db/CrestGuides/Crest_Goldola.png" />
+                <br/>
+                BOSS: Gorondola <br />
+
+                <b>Appears on Day: Thursday [PST]</b> <br/>
+                HP: 5,200,000 <br />
+                Element: Earth <br />
+                Race: Spirit <br />
+            </center><br />
+            <h3><center>Mechanics</center></h3>
+            <b><u>Special Mobs<b><u>
+            <ul>
+                <li>Dulah: Chance to inflict Seal with Skill.</li>
+                <li>Logbord: Reduces Enemies’ DEF with Skill.</li>
+                <li>Cocogran: Reduces Enemies’ Action Speed with Skill.</li>
+                <li>Ragvelon: Reduces Enemies’ Arts by 20 with Skill.</li>
+                <li>Moldola: Chance to inflict Poison with Skill.</li>
+                <li>Spirit Meiol: Heal Allies’ HP by 20000 with Skill.</li>
+                <li>King Mokyuon: Chance to inflict Curse with Skill.</li>
+            </ul>
+            <p>These mobs will appear in the pre-stages leading up to the boss, and the boss will summon them 
+            throughout the battle. Ragvelon’s reduction of Enemies’ Arts by 20 makes taunt tanks incredibly useful 
+            in this stage, while the mobs that inflict status ailments mean a way to mitigate status ailments is essential for clearing this boss.</p>
+
+            <h4><center><u>Normal Attacks</u></center></h4>
+            <b>Used during Storm/Ultra Strom Mode:</b>
+            <ul>
+                <li>Jumps up, lands, and releases a purple mist - Chance to Poison enemies</li>
+                <ul>
+                    <li>Not much of an issue unless you aren’t bringing a way to cleanse it.</li>
+                </ul>
+                <li>Sucks in air - Reduces Enemies’ Arts by 100</li>
+                <ul>
+                    <li>Without a taunt tank, this would take 100 Arts from every member of your team, once again reinforcing the usefulness of taunt tanks for this boss.</li>
+                </ul>
+            </ul>
+
+            <center><h4><u>Arts</u></h4></center>
+
+            <ul>
+                <li><b>Chewing Rose</b> - Heals own HP by 350 each time this Arts hits an Enemy</li>
+                <ul>
+                    <li>The healing is fairly insignificant, so it’s simply a matter of not dying to the raw damage.</li>
+                </ul>
+            </ul>
+
+            <h3><center>Strategy</center></h3>
+            <p><center>
+            An integral part of surviving through the boss stage is managing the large amount of ailments 
+            Gorondola and his minions inflict on your party. This is preferably done with the use of 
+            Taunt-Tanks, but units capable of applying Status RES are also more than capable given you 
+            can properly loop them. It’s also important to remember that the boss deals rather potent damage, 
+            so slacking on sustain would be highly detrimental.
+            </p></center>
+            <b><h4><u>Recommended Units</u></h4></b>
+            <ul>
+                <li>
+                    <b>Thetis</b>
+                    <p>An obvious top choice for your taunt-tank. Thetis is more than capable of 
+                    redirecting all Ailments onto himself not to mention gaining immunity to the 
+                    most irritating of them given he is equipped with his True Weapon. Proper use 
+                    of DEF equipment will also allow him to tank all the damage no problem.</i>
+                    </p>
+                </li>
+                <li>
+                    <b>Gerald/Naofumi &gt; Celia &gt; Dark Zeorg</b>
+                    <p>Similarly with Thetis, proper management with the use of the rest of the 
+                    taunt-tanks is more than able of sustaining and eating all ailments inflicted 
+                    by enemies. Though as they lack innate Seal(Silence) RES unlike Thetis with 
+                    his True Weapon, you will need to bring additional units/equipment capable 
+                    of cleansing ailments off them.</p>
+                </li>
+                <li>
+                    <b>Feena/Rimuru</b>
+                    <p>An alternative to taunt-tanks with the use of their Status RES buffs. 
+                    It’s worth mentioning that while they’re capable of resisting ailments, 
+                    they are unable to mitigate the Arts drain of Gorondola and Ragvelon. 
+                    This leaves your whole team more susceptible to being disrupted, potentially 
+                    when you require an Arts use the most. They are still, however, very strong 
+                    units with a proper team comp.</p>
+                </li>
+        `,
+        author: "ebouliben#1234, Jeffers#7056"
     }
 ]
 
