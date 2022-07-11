621cbaa3a363c658542ee4942de1b817e2a3d7ea
John Nguyen <aznxmist@gmail.com>
2022-07-11 12:38:16 -0500

    Fixed top down spacing for Home link

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -59,7 +59,11 @@ return (
                                 <NavItem>
                                     <NavLink className="nav-link" to="/" activeClassName="active" onClick={() => { setIsOpen(!isOpen) }}>
                                         <Media src="/chara_face_1200_1.png" style={{maxHeight: "35px", marginTop: "0rem"}} />
-                                        <b>HOME</b>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/" activeClassName="active" onClick={() => { setIsOpen(!isOpen) }}>
+                                        <b><p style={{paddingTop: "3px", marginBottom: "0rem"}}>HOME</p></b>
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
