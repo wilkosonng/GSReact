d62c5e82b1d1047095fa757bb170f6ef86430bf8
John Nguyen <aznxmist@gmail.com>
2022-07-19 12:45:36 -0500

    Changed Card structure to prevent any margins or padding

--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -14,58 +14,42 @@ export default function News () {
     return (
         <Container>
             <Row>
-                <Card style={{backgroundColor: "#29648a"}}>
-                    <CardHeader>
+                <Card style={{backgroundColor: "#29648a", padding: "0"}}>
+                    <CardHeader style={{padding: "0px"}}>
                         <h1><center><strong>Site News</strong></center></h1>
                     </CardHeader>
                 </Card>
             </Row>
             <Row>
                 <Card style={{backgroundColor:"#464866", color: "#C5C6C7"}}>
-                    <CardBody>
+                    <CardBody style={{padding: "0px", height: "100%", color: "#25274d"}}>
                         <Row>
-                            <Col>
-                                <Card style={{backgroundColor:"#464866", color: "#C5C6C7", border: "none"}}>
-                                    {newsTopics.map(theNews => {
+                            <CardGroup style={{padding: "0px"}}>
+                                {newsTopics.map(theNews => {
                                         if(theNews.new){
                                             return (
                                                 <React.Fragment key={theNews.id}>
-                                                    <Col>
-                                                            <Link to={`/news/${theNews.id}`} style={{textDecoration: 'none'}} key={theNews.id}>
-                                                                <Card style={{maxWidth: "1000rem", width: "100%", height: "auto", maxHeight: "700px" ,backgroundColor: "#25274d", color: "#29648a", marginBottom: "2rem"}}>
-                                                                    <CardHeader  style={{backgroundColor: "#29648a", color: "#aaabb8", padding: "0px"}}>
-                                                                        <Container>
-                                                                        <Row>
-                                                                            <Col lg="auto">
-                                                                                <p style={{fontSize: "20px", marginLeft: "0rem", paddingBottom: "0px"}}><strong>[{theNews.category}]</strong></p>
-                                                                                <p style={{}}><center><h4>{theNews.title}</h4></center></p>
-                                                                            </Col>
-                                                                            <Col lg="auto">
-                                                                                <p style={{float: "right"}}>{theNews.date}</p>
-                                                                            </Col>
-                                                                        </Row>
-                                                                        </Container>
-                                                                    </CardHeader>
-                                                                    <CardBody>
-                                                                        <center>
-                                                                            <img src={theNews.preview} alt="Preview Image" style={{maxWidth: "200px", width: "auto", height: "auto", maxHeight: "400px"}} />
-                                                                            <p style={{backgroundColor: "#29648a", color: "#aaabb8", maxWidth: "300px", width: "100%", height: "auto",marginTop: "2rem"}}>Read Article</p>
-                                                                        </center>
-                                                                    </CardBody>
-                                                                </Card>
-                                                            </Link>
-                                                    </Col>
+                                                    <Card style={{maxWidth: "1000rem", width: "100%", height: "500px", maxHeight: "700px" ,backgroundColor: "#25274d", color: "#29648a", padding: "0px", margin: "0px"}}>
+                                                        <CardHeader style={{backgroundColor: "#29648a", color: "#aaabb8", padding: "0px", height: "80px"}}>
+                                                        <Link to={`/news/${theNews.id}`} style={{textDecoration: 'none', color: "#aaabb8"}} key={theNews.id}><center><h2>{theNews.title}</h2></center></Link>
+                                                        </CardHeader>
+                                                        <CardBody style={{marginTop: "30px"}}>
+                                                            <center>
+                                                                <img src={theNews.preview} alt="Preview Image" style={{maxWidth: "200px", width: "auto", height: "auto", maxHeight: "400px"}} />
+                                                                <Link to={`/news/${theNews.id}`} style={{textDecoration: 'none', color: "#aaabb8"}} key={theNews.id}>
+                                                                    <p style={{backgroundColor: "#29648a", color: "#aaabb8", maxWidth: "300px", width: "100%", height: "auto",marginTop: "2rem"}}>Read Article</p>
+                                                                </Link>
+                                                            </center>
+                                                        </CardBody>
+                                                    </Card>
                                                 </React.Fragment>
                                             )
                                         }
 
                                     })}
-                                </Card>
-                            </Col>
-                            <Col>
-                                <Card style={{backgroundColor: "#25274d", color: "#29648a", maxHeight: "500px"}}>
-                                    <CardHeader style={{backgroundColor: "#29648a", color: "#aaabb8"}}><h2><center>Twitter Feed</center></h2></CardHeader>
-                                    <CardBody>
+                                <Card style={{backgroundColor: "#25274d", color: "#29648a", height: "500px", width: "100%", padding: "0px", margin: "0px"}}>
+                                    <CardHeader style={{backgroundColor: "#29648a", color: "#aaabb8", padding: "0", height: "80px"}}><h2 style={{paddingTop: "15px"}}><center>Twitter Feed</center></h2></CardHeader>
+                                    <CardBody style={{marginTop: "15px"}}>
                                         <center>
                                             <TwitterTimelineEmbed
                                             sourceType="profile"
@@ -77,23 +61,24 @@ export default function News () {
                                         </center>
                                     </CardBody>
                                 </Card>
-                            </Col>
+                            </CardGroup>
                         </Row>
-                        <Row>
-                            <Card style={{backgroundColor: "#25274d", color: "#29648a", maxHeight: "500px"}}>
-                                <CardHeader style={{backgroundColor: "#29648a", color: "#aaabb8"}}><center><h2>Discord</h2></center></CardHeader>
-                                <CardBody>
-                                    <center>
-                                        Visit the Grand Summoners Official Discord for older news and Unit Reviews!
-                                        <br />
-                                        <a href="https://discord.gg/grandsummoners" id="discordLink">Click Here</a>
-                                    </center>
-                                </CardBody>
-                            </Card>
-                        </Row>
+                        
                     </CardBody>
                 </Card>
-            </Row>
+                </Row>
+                <Row>
+                    <Card style={{backgroundColor: "#25274d", color: "#29648a", padding: "0px"}}>
+                        <CardHeader style={{backgroundColor: "#29648a", color: "#aaabb8", width: "100%", padding: "0px", margin: "0px"}}><center><h2>Discord</h2></center></CardHeader>
+                        <CardBody style={{width :"100%", margin: "10px"}}>
+                            <center>
+                                Visit the Grand Summoners Official Discord for older news and Unit Reviews!
+                                <br />
+                                <a href="https://discord.gg/grandsummoners" id="discordLink">Click Here</a>
+                            </center>
+                        </CardBody>
+                    </Card>
+                </Row>
         </Container>
     )
 }
