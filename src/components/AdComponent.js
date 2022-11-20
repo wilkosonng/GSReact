beeee6d825d478b2363b8505236154ef77b568ce
John Nguyen <aznxmist@gmail.com>
2022-11-20 07:52:45 -0600

    Added dimensions for Mobile. Reduced height

--- src/components/AdComponent.js
+++ src/components/AdComponent.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Mobile, Default } from './MobileCheckComponent'
+import { Container } from 'reactstrap';
 
 class Adsense extends Component {
 
@@ -8,14 +10,29 @@ class Adsense extends Component {
 
     render() {
         return (
-                <ins className='adsbygoogle'
-                    style={{ display: 'inline-block', paddingTop: "2rem", maxHeight: "200px", width: "100%" }}
+            <Container>
+                <Default>
+                    <ins className='adsbygoogle'
+                        style={{ display: 'inline-block', paddingTop: "2rem", maxHeight: "120px", width: "100%" }}
+                        data-ad-client= 'ca-pub-7386611028385098'
+                        data-ad-slot='9172261328'
+                        data-ad-format= 'auto'
+                        data-full-width-responsive="true"
+                    >
+                    </ins>
+                </Default>
+                <Mobile>
+                    <ins className='adsbygoogle'
+                    style={{ display: 'inline-block', paddingTop: "2rem", maxHeight: "120px", width: "100%", maxWidth: "280px" }}
                     data-ad-client= 'ca-pub-7386611028385098'
                     data-ad-slot='9172261328'
                     data-ad-format= 'auto'
                     data-full-width-responsive="true"
-                >
-                </ins>
+                    >
+                    </ins>
+                </Mobile>
+            </Container>
+
         );
     }
 }
