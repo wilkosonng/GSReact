3d3c97a08981599c9d5eb91b3adefedfb8135f73
John Nguyen <aznxmist@gmail.com>
2022-12-12 13:06:31 -0600

    Added Luck Unit Tier List

--- src/components/TierlistComponent.js
+++ src/components/TierlistComponent.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Container, Row, Col, Card, CardHeader, CardBody, CardGroup, UncontrolledAccordion, AccordionHeader, AccordionItem, AccordionBody } from 'reactstrap';
+import classNames from 'classnames';
+import React, {useState} from 'react';
+import { Container, Row, Col, Card, CardHeader, CardBody, CardGroup, UncontrolledAccordion, AccordionHeader, AccordionItem, AccordionBody,
+            TabContent, TabPane, Nav, NavItem, NavLink} from 'reactstrap';
 import { getUnits } from '../shared/unitInfo';
 import { Default, Mobile } from './MobileCheckComponent';
 
@@ -15,6 +17,13 @@ const accordionColor = {
 }
 
 export default function Tierlist() {
+    //Tab hook
+    const [currentTab, setCurrentTab] = useState('1');
+
+    const toggle = tab => {
+        if (currentTab !== tab) setCurrentTab(tab);
+    }
+
     //Obtain Unit Data
     const units = getUnits();
     
@@ -22,7 +31,32 @@ export default function Tierlist() {
         <Container style={{marginTop: "5rem"}}>
             <DisplayHeader />
             <Row>
-                <DisplayAccordion unit={units} />
+                <Nav pills justified>
+                    <NavItem>
+                        <NavLink 
+                            className={classNames({ active: currentTab === '1' })}
+                            onClick={() => { toggle('1') }}
+                            >
+                            All Units
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink 
+                            className={classNames({ active: currentTab === '2' })}
+                            onClick={() => { toggle('2') }}
+                            >
+                            Luck Units
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent activeTab={currentTab}>
+                    <TabPane tabId="1">
+                        <DisplayAccordion unit={units} />
+                    </TabPane>
+                    <TabPane tabId="2">
+                        <DisplayLuck unit={units} />
+                    </TabPane>
+                </TabContent>
             </Row>
         </Container>
     )
@@ -31,7 +65,7 @@ export default function Tierlist() {
 function DisplayHeader(){
     return (
         <Row>
-            <Card style={{backgroundColor: "#292930", color: "#e7f9fc", padding: "0px"}}>
+            <Card style={{backgroundColor: "#292930", color: "#e7f9fc", padding: "0px", marginBottom: "2rem"}}>
                 <CardHeader>
                     <h1><center><strong>Global Tierlist</strong></center></h1>
                 </CardHeader>
@@ -56,6 +90,7 @@ function DisplayHeader(){
                                 <p><center>This tierlist is for GLOBAL units only. Do <strong>not</strong> consider this gospel. Each unit may have their own niche, even the lower ranking units</center></p>
                                 <p><center>This is simply meant to show the general analysis of each unit. </center></p>
                                 <p><center>Rankings were determined by players from the Grand Summoners <a href="https://discord.gg/grandsummoners">Official Discord</a> Server Staff.</center></p>
+                                <p><center>Be aware that the Rankings are based on their <b>most optimized</b> scenarios.</center></p>
                             </CardBody>
                         </Card>
                 </CardBody>
@@ -84,7 +119,18 @@ function DisplayAccordion({unit}) {
 }
 
 function DisplayLuck({unit}) {
-
+    return (
+        <UncontrolledAccordion flush defaultOpen={["SSS", "SS", "S", "A", "B", "C", "F", "TBD"]} stayOpen style={{marginTop: "3rem", width: "100%"}}>
+            <DisplayLuckRank unit={unit} rank="SSS" />
+            <DisplayLuckRank unit={unit} rank="SS" />
+            <DisplayLuckRank unit={unit} rank="S" />
+            <DisplayLuckRank unit={unit} rank="A" />
+            <DisplayLuckRank unit={unit} rank="B" />
+            <DisplayLuckRank unit={unit} rank="C" />
+            <DisplayLuckRank unit={unit} rank="F" />
+            <DisplayLuckRank unit={unit} rank="TBD" />
+        </UncontrolledAccordion>
+    )
 }
 
 function DisplayRank({unit, rank}) {
@@ -216,6 +262,134 @@ function DisplayRank({unit, rank}) {
     )
 }
 
+function DisplayLuckRank({unit, rank}) {
+    const desktopWidth = "80%";//"75px";
+    const mobileWidth = "60%";//"60px";
+    const desktopFont = "15px";
+    const mobileFont = "12px";
+
+    return (
+        <>
+            <AccordionHeader targetId={rank}>
+                <h1><strong>{rank}</strong></h1>
+            </AccordionHeader>
+            <AccordionItem>
+                <AccordionBody accordionId={rank} style={accordionColor}>
+                    <CardGroup>
+                        <Default>
+                            {unit.map(units => {
+                                if(units.luckrank === rank && units.attribute === "Fire"){
+                                    return (
+                                        <DisplayUnit unit={units} imageSize={desktopWidth} fontSizing={desktopFont}/>
+                                    )
+                                }
+                                return ""
+                            })}
+                            {unit.map(units => {
+                                if(units.luckrank === rank && units.attribute === "Earth"){
+                                    return (
+                                        <DisplayUnit unit={units} imageSize={desktopWidth} fontSizing={desktopFont}/>
+                                    )
+                                }
+                                return ""
+                            })}
+                            {unit.map(units => {
+                                if(units.luckrank === rank && units.attribute === "Water"){
+                                    return (
+                                        <DisplayUnit unit={units} imageSize={desktopWidth} fontSizing={desktopFont}/>
+                                    )
+                                }
+                                return ""
+                            })}
+                            {unit.map(units => {
+                                if(units.luckrank === rank && units.attribute === "Light"){
+                                    return (
+                                        <DisplayUnit unit={units} imageSize={desktopWidth} fontSizing={desktopFont}/>
+                                    )
+                                }
+                                return ""
+                            })}
+                            {unit.map(units => {
+                                if(units.luckrank === rank && units.attribute === "Dark"){
+                                    return (
+                                        <DisplayUnit unit={units} imageSize={desktopWidth} fontSizing={desktopFont}/>
+                                    )
+                                }
+                                return ""
+                            })}
+                        </Default>
+                        <Mobile>
+                            <Container>
+                                <Row>
+                                {unit.map((units) => {
+                                    if(units.luckrank === rank && units.attribute === "Fire"){
+                                        return (
+                                            <>
+                                                <Col xs="4">
+                                                    <DisplayUnit unit={units} imageSize={mobileWidth} fontSizing={mobileFont}/>
+                                                </Col>
+                                            </>
+                                        )
+                                    }
+                                    return ""
+                                })}
+                                {unit.map((units) => {
+                                    if(units.luckrank === rank && units.attribute === "Earth"){
+                                        return (
+                                            <>
+                                                <Col xs="4">
+                                                    <DisplayUnit unit={units} imageSize={mobileWidth} fontSizing={mobileFont}/>
+                                                </Col>
+                                            </>
+                                        )
+                                    }
+                                    return ""
+                                })}
+                                {unit.map((units) => {
+                                    if(units.luckrank === rank && units.attribute === "Water"){
+                                        return (
+                                            <>
+                                                <Col xs="4">
+                                                    <DisplayUnit unit={units} imageSize={mobileWidth} fontSizing={mobileFont}/>
+                                                </Col>
+                                            </>
+                                        )
+                                    }
+                                    return ""
+                                })}
+                                {unit.map((units) => {
+                                    if(units.luckrank === rank && units.attribute === "Light"){
+                                        return (
+                                            <>
+                                                <Col xs="4">
+                                                    <DisplayUnit unit={units} imageSize={mobileWidth} fontSizing={mobileFont}/>
+                                                </Col>
+                                            </>
+                                        )
+                                    }
+                                    return ""
+                                })}
+                                {unit.map((units) => {
+                                    if(units.luckrank === rank && units.attribute === "Dark"){
+                                        return (
+                                            <>
+                                                <Col xs="4">
+                                                    <DisplayUnit unit={units} imageSize={mobileWidth} fontSizing={mobileFont}/>
+                                                </Col>
+                                            </>
+                                        )
+                                    }
+                                    return ""
+                                })}
+                                </Row>
+                            </Container>
+                        </Mobile>
+                    </CardGroup>
+                </AccordionBody>
+            </AccordionItem>
+        </>
+    )
+}
 
 function DisplayUnit({unit, imageSize, fontSizing}) {
 
