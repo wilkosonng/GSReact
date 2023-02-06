16085f48273208c6fb85f2a58f1fa27bc430e659
Reymdusk <aznxmist@gmail.com>
2023-02-06 11:05:49 -0500

    Displays that Lore is unavailable if section is incomplete

--- src/components/equipment/EquipLore.js
+++ src/components/equipment/EquipLore.js
@@ -1,6 +1,13 @@
+const pStyle = {
+    marginTop: "3rem",
+    marginLeft: "10%",
+    marginRight: "10%"
+}
 const EquipLore = ({ lore }) => {
     return (
-            <p style={{marginTop: "3rem"}}>{lore}</p>
+        <>
+            {lore ? <p style={pStyle}>{lore}</p> : <p style={pStyle}>Lore is currently unavailable</p>}
+        </>
     )
 }
 
