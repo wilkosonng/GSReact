1bc6508a572bacb6864556201b6c536ccd490da4
Reymdusk <aznxmist@gmail.com>
2023-03-14 15:18:48 -0400

    Added unit name for if image is missing

--- src/components/TierlistComponent.js
+++ src/components/TierlistComponent.js
@@ -468,7 +468,7 @@ function DisplayUnit({unit, imageSize, fontSizing}) {
         <center>
             <Card style={{border: "none", width: "90px", backgroundColor: "rgba(0, 0, 0, 0)", padding: "0px" , margin: "0px"}}>
                 <a href={`/units/${unit.name}/`} style={{textDecoration: "none"}}>
-                    <img src={unit.image.thumbawk} style={thumbnailSize}  />
+                    <img src={unit.image.thumbawk} style={thumbnailSize}  alt={unit.name}/>
                     <p style={{paddingBottom: "0px", marginBottom: "2px", color: "#aaabb8", fontSize: `${fontSizing}`, width: "75px", display: "inline-block"}}>{unit.name}</p>
                 </a>
             </Card>
